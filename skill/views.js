964b4be97e0402242694457ee8837ab1d39234fc
Rommel Berrios <rommel.berrios93@gmail.com>
2017-08-30 00:36:47 -0600

    Update views

--- skill/views.js
+++ skill/views.js
@@ -5,30 +5,30 @@ const _ = require('lodash');
 
 const views = {
   // LaunchIntent
-  'launch.welcome.ask': 'Hi welcome. can i help you?',
-  'launch.welcome.reprompt': 'Can i help you?',
+  'launch.welcome.ask': 'Hi welcome to tab control. You can create a tab or consult existing tabs. How can i help you today?',
+  'launch.welcome.reprompt': 'How may i help you today?',
 
   // Trivia Intent
-  'help.general.ask': 'help. can i help you?',
-  'help.general.reprompt': 'can i help you?',
+  'help.general.ask': 'Tab control will help you manage all your tabs. You can say things like. Alexa, add $5 to Ava\'s tab or Alexa, What is Ava\'s tabs',
+  'help.general.reprompt': 'What i can do for you?',
 
-  'tab.addToTab.ask': 'Great, i added ${amount} to {person}\'s tabs. Can i help you with something else?',
-  'tab.addToTab.reprompt': 'can i help you with something else?',
+  'tab.addToTab.ask': 'Great, i added ${amount} to {person}\'s tabs. Is there something else i can help you with?',
+  'tab.addToTab.reprompt': 'Is there something else i can help you with?',
 
-  'tab.askForName.ask': 'i need a name. do you want to add more?',
-  'tab.askForName.reprompt': 'please provide a name',
+  'tab.askForName.ask': 'Please specify a name.',
+  'tab.askForName.reprompt': 'please provide a name so i can create a new tab or add to an existing tab',
 
-  'tab.askForAmount.ask': 'i need the amount. do you want to add more?',
-  'tab.askForAmount.reprompt': 'please tell how much',
+  'tab.askForAmount.ask': 'Tell me the amount you want to add',
+  'tab.askForAmount.reprompt': 'Please tell me how much you are going to add to that tab',
 
-  'tab.askForNameOnStatus.ask': 'i need a name. please specify a name',
-  'tab.askForNameOnStatus.reprompt': 'please specify a name',
+  'tab.askForNameOnStatus.ask': 'Please specify a name.',
+  'tab.askForNameOnStatus.reprompt': 'Tell me which tab you want to check',
 
-  'tab.statusNoAmount.ask': 'Tab is empty. Is there something else i can help you with?',
+  'tab.statusNoAmount.ask': 'i\'m sorry but this tab is empty. Is there something else i can help you with?',
   'tab.statusNoAmount.reprompt': 'Is there something else i can help you with?',
 
-  'tab.status.ask': 'Let\'s see. {person}\'s tab is ${amount}. can i help you with something else?',
-  'tab.status.repromt': 'can i help you with something else?',
+  'tab.status.ask': 'Let\'s see. {person}\'s tab is ${amount}. Is there something else i can help you with?',
+  'tab.status.repromt': 'Is there something else i can help you with?',
 };
 
 // Compile views
